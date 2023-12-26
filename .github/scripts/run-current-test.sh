b839e279b3bae00610629815dd85abbaaa9a3577
Juan Longo <juan.longo@mulesoft.com>
2023-12-26 17:11:18 -0300

    WIP 11

--- .github/scripts/run-current-test.sh
+++ .github/scripts/run-current-test.sh
@@ -4,7 +4,7 @@ current_tp=$(sed -n '2p' ./current_tp.txt)
 
 # Generate the string based on the value of second_line
 result=""
-for ((i=1; i<=current_tp; i++)); do
+for ((i=1; i<=$current_tp; i++)); do
   result+="exercises/practice/tp-$i "
 done
 
