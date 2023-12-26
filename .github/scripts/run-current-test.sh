d910bc84291b2babb675d4bea3f08b0d53ca89a8
Juan Longo <juan.longo@mulesoft.com>
2023-12-26 17:08:15 -0300

    WIP 9

---
+++ .github/scripts/run-current-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Read the second line from the file
+current_tp=$(sed -n '2p' ./current_tp.txt)
+
+# Generate the string based on the value of second_line
+result=""
+for ((i=1; i<=second_line; i++)); do
+  result+="exercises/practice/tp-$i "
+done
+
+pytest $result
